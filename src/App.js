1138e5d54c765cf65ef20c367b671ed503df3705
EmirJavascript <orozbekovjob@gmail.com>
2023-03-09 18:08:54 +0600

    done but going to refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [status, setStatus] = useState('day')
+  const changeTime = () => {
+    setStatus(!status) 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${status ? "day" : "night"}`} onClick={changeTime}>
+      <div className="sun-moon"></div>
     </div>
   );
 }
